dd37cb6fa65bf3b86ff70dc00d5061a55ecb7873
johnkonderla <konderla.john@gmail.om>
2017-05-20 16:25:49 -0400

    Initial Commit

---
+++ yuri/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  yuri
+//
+//  Created by John Konderla on 5/20/17.
+//  Copyright © 2017 John Konderla. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
